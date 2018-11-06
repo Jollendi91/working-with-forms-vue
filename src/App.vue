1c1cf1ce51b38a899c3d115365336cc06fdab75f
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-11-06 13:16:49 -0600

    create custom inputs

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if="!isSubmitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <!-- Exercise 1 -->
@@ -9,11 +9,29 @@
                     <!-- Mail -->
                     <!-- Password -->
                     <!-- Store Data? Yes/No -->
+                    <form-full-name v-model="fullName"></form-full-name>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input type="email" id="email" class="form-control" v-model="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" class="form-control" v-model="password">
+                    </div>
+                    <div class="form-group">
+                        <p>Store Data?</p>
+                        <label for="yes">
+                            <input type="radio" id="yes" value="Yes" v-model="storeData">Yes
+                        </label>
+                        <label for="no">
+                            <input type="radio" id="no" value="No" v-model="storeData">No
+                        </label>
+                    </div>
 
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
                     <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
+                    <button @click.prevent="isSubmitted = true">Submit</button>
                     <!-- Exercise 3 -->
                     <!-- Edit the Example from above and create a custom "Full Name" Control -->
                     <!-- which still holds the First Name and Last Name Input Field -->
@@ -21,17 +39,17 @@
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ fullName }}</p>
+                        <p>Mail: {{ email }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
                     </div>
                 </div>
             </div>
@@ -40,7 +58,21 @@
 </template>
 
 <script>
+    import FullName from './FullName.vue';
+
     export default {
+        data() {
+            return {
+                fullName: 'Joshua Ollendick',
+                email: '',
+                password: '',
+                storeData: 'No',
+                isSubmitted: false
+            }
+        },
+        components: {
+            formFullName: FullName
+        }
     }
 </script>
 
